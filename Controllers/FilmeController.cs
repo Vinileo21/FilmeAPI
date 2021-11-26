61196f8907f68d6f63cf31da80b698de98fc56d3
Vinicius Leonardo <88686165+Vinileo21@users.noreply.github.com>
2021-11-26 10:27:30 -0300

    Add files via upload

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,43 @@
+﻿using FilmeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]//criar um recurso    
+        public IActionResult AdicionarFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            filme.Id = id++;
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);        
+        }
+
+        [HttpGet]//recuperar recurso
+        public IActionResult RecuperarFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmesPorId(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+        
+    }
+}
---
+++ Data/FilmeContext.cs
@@ -0,0 +1,23 @@
+﻿using FilmeAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmeAPI.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> opt) : base (opt)
+        {
+
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=filmeDb;user=root;password=root");
+        }
+    }
+}
